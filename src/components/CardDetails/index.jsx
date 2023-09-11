44ad2c5e2279f70f2994feac14d87f186929c300
Amir Maleki <realmalekiamir@gmail.com>
2023-09-11 15:16:41 +0330

    Add moment jalali package

--- src/components/CardDetails/index.jsx
+++ src/components/CardDetails/index.jsx
@@ -1,28 +1,33 @@
 import { AiOutlineEye, AiOutlineComment, AiOutlineHeart } from 'react-icons/ai';
 import { IoShareOutline } from 'react-icons/io5';
+import convertToPersianNumber from '../../utils/convertToPersianNumber';
 
-function CardDetails() {
+function CardDetails({ text, timestamp }) {
   return (
-    <div className="w-full flex items-center justify-between px-2">
-      <div className="flex items-center gap-1">
-        <span>1</span>
-        <AiOutlineHeart className="w-6 h-6" />
+    <div>
+      <div className="w-full flex items-center justify-between p-2">
+        <div className="flex items-center gap-1">
+          <span>{convertToPersianNumber(1)}</span>
+          <AiOutlineHeart className="w-6 h-6" />
+        </div>
+
+        <div className="flex items-center gap-1">
+          <span>{convertToPersianNumber(1)}</span>
+          <AiOutlineComment className="w-6 h-6" />
+        </div>
+
+        <div className="flex items-center gap-1">
+          <span>{convertToPersianNumber(0)}</span>
+          <IoShareOutline className="w-6 h-6" />
+        </div>
+
+        <div className="flex items-center gap-1">
+          <span>{convertToPersianNumber(14)}</span>
+          <AiOutlineEye className="w-6 h-6" />
+        </div>
       </div>
 
-      <div className="flex items-center gap-1">
-        <span>1</span>
-        <AiOutlineComment className="w-6 h-6" />
-      </div>
-
-      <div className="flex items-center gap-1">
-        <span>0</span>
-        <IoShareOutline className="w-6 h-6" />
-      </div>
-
-      <div className="flex items-center gap-1">
-        <span>14</span>
-        <AiOutlineEye className="w-6 h-6" />
-      </div>
+      {/* time */}
     </div>
   );
 }
