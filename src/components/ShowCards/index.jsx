8e7c745e0d55a8abf939f264002fedafab8f892a
Amir Maleki <realmalekiamir@gmail.com>
2023-09-11 12:59:46 +0330

    Fetch posts using useEffect hook and fetchPosts function

--- src/components/ShowCards/index.jsx
+++ src/components/ShowCards/index.jsx
@@ -1,4 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { fetchPosts } from '../../services/api';
+
 function ShowCards() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchUserList() {
+      try {
+        setLoading(true);
+        const userData = await fetchPosts();
+        setPosts(userData);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false); // Hide loading indicator regardless of success or failure
+      }
+    }
+
+    fetchUserList();
+  }, []);
+
   return <div></div>;
 }
 
