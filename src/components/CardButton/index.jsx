6a04fa373fb25a73aa7cc38186d894fab0e421b2
Amir Maleki <realmalekiamir@gmail.com>
2023-09-11 15:39:23 +0330

    Create CardButton component

---
+++ src/components/CardButton/index.jsx
@@ -0,0 +1,5 @@
+function CardButton({ id }) {
+  return <div className=""></div>;
+}
+
+export default CardButton;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Card } from './Card/index';
 export { default as CardHeader } from './CardHeader/index';
 export { default as CardImage } from './CardImage/index';
 export { default as CardDetails } from './CardDetails/index';
+export { default as CardButton } from './CardButton/index';
