5d6d90cda3f97f951a6f8bc1444491e9523e58d8
Amir Maleki <realmalekiamir@gmail.com>
2023-09-11 16:28:52 +0330

    Add two shadow custom class to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        card: 'rgba(17,17,26,0.1) 0px 0px 16px',
+        'card-lg':
+          '0px 4px 16px rgba(17, 17, 26, 0.1), 0px 8px 24px rgba(17, 17, 26, 0.1), 0px 16px 56px rgba(17, 17, 26, 0.1)',
+      },
+    },
   },
   plugins: [],
-}
-
+};
